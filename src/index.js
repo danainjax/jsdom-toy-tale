1cdd196fc23c46c071de711e88254a8d5aa5952f
danainjax <danainjax@gmail.com>
2021-06-03 09:33:06 -0400

    coded along with Evans Wang video

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
+const addBtn = document.querySelector("#new-toy-btn");
+const toyFormContainer = document.querySelector(".container");
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
+  console.log("%cThe dom is loaded.", "color :blue")
+  fetch("http://localhost:3000/toys")
+  .then(response => response.json())
+  .then(toys => {
+    console.log(toys)
+    let toysHTML = toys.map(function(object) {
+      return `
+    <div class="card">
+      <h2>${object.name}</h2>
+      <img src=${object.image} class="toy-avatar" />
+      <p>${object.likes}</p>
+      <button class="like-btn">Like</button>
+    </div>
+      `
+    })
+    document.querySelector("#toy-collection").innerHTML += toysHTML.join("")
+  })
+
+  toyFormContainer.addEventListener("submit", function(event) {
+    event.preventDefault()
+    
+    const toyName = event.target.name.value
+    const toyImage = event.target.image.value
+    console.log(toyName, toyImage);
+  })
+
   addBtn.addEventListener("click", () => {
+    console.log("%cYou clicked the button.", "color :red")
     // hide & seek with the form
     addToy = !addToy;
     if (addToy) {
@@ -12,4 +39,6 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+
 });
