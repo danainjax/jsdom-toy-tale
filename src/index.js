0a473801727a3be9a4964281161a2b7c4789ae32
danainjax <danainjax@gmail.com>
2021-06-05 17:11:32 -0400

    still refreshing, getting assistance

--- src/index.js
+++ src/index.js
@@ -3,15 +3,72 @@ const toyForm = document.querySelector(".container");
 let addToy = false;
 
 // YOUR CODE HERE
-  
+  document.addEventListener("DOMContentLoaded", ()=>{
+
+    fetch('http://localhost:3000/toys')
+    .then(r => r.json())
+    .then(toys => {
+      
+      //take toys array and make into HTML in order to add to the DOM
+      let toysHTML = toys.map(function(toy){
+        return `
+        <div class="card">
+          <h2>${toy.name}</h2>
+          <img src=${toy.image} class="toy-avatar" />
+          <p>${toy.likes} Likes </p>
+          <button class="like-btn">Like <3</button>
+        </div>
+
+        `
+      })
+      document.querySelector('#toy-collection').innerHTML += toysHTML.join('')
+    })
+
+    toyForm.addEventListener("submit", function(event){
+      
+      event.preventDefault();
+      const toyName = event.target.name.value
+      const toyImage = event.target.image.value
+
+      fetch("http://localhost:3000/toys", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+
+        body: JSON.stringify({
+          name: toyName,
+          image: toyImage,
+          likes: 0
+        })
+      })
+      .then( r => r.json())
+      .then ( newToy => {
+        //convert new toy from JSON to HTML to add to the DOM
+        let newToyHTML = `
+      <div class="card">
+        <h2>${newToy.name}</h2>
+        <img src=${newToy.image} class="toy-avatar" />
+        <p>${newToy.likes}</p>
+        <button data id=${newToy.id} class="like-btn">Like</button>
+    </div>
+
+      `
+      document.querySelector('#toy-collection').innerHTML += newToyHTML
+      // toyCollection.innerHTML += newToyHTML.join("")
+      }) 
+    })
+    addBtn.addEventListener("click", () => {
+      // hide & seek with the form
+      
+      addToy = !addToy;
+      if (addToy) {
+        toyForm.style.display = "block";
+      } else {
+        toyForm.style.display = "none";
+      }
+    })
+  })
 
 
-addBtn.addEventListener("click", () => {
-  // hide & seek with the form
-  addToy = !addToy;
-  if (addToy) {
-    toyForm.style.display = "block";
-  } else {
-    toyForm.style.display = "none";
-  }
-})
