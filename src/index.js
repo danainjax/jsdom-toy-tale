5e318ee0ac0988b459722b987e888df2e1b8803f
danainjax <danainjax@gmail.com>
2021-06-06 09:50:19 -0400

    still broken and refreshing, likes increment not working

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const addBtn = document.querySelector("#new-toy-btn");
 const toyForm = document.querySelector(".container");
 const toyCollection = document.querySelector('#toy-collection')
 let addToy = false;
-
+// document.querySelector("#form")
 // YOUR CODE HERE
   document.addEventListener("DOMContentLoaded", ()=>{
 
@@ -17,21 +17,20 @@ let addToy = false;
           <h2>${toy.name}</h2>
           <img src=${toy.image} class="toy-avatar" />
           <p>${toy.likes} Likes </p>
-          <button class="like-btn">Like <3</button>
+          <button data-id="${toy.id}" class="like-btn">Like <3</button>
         </div>
 
         `
+        console.log(toy)
       })
       toyCollection.innerHTML += toysHTML.join('')
+      console.log(toyCollection)
     })
 
-    toyForm.addEventListener("submit", function(event){
-      
-      event.preventDefault();
-      console.log('We got past the event prevent default line')
-      // debugger;
-      const toyName = event.target.name.value
-      const toyImage = event.target.image.value
+    
+    function newToy(toy) {
+      const toyName = toy.name.value
+      const toyImage = toy.image.value
 
       fetch("http://localhost:3000/toys", {
         method: "POST",
@@ -54,24 +53,57 @@ let addToy = false;
         <h2>${newToy.name}</h2>
         <img src=${newToy.image} class="toy-avatar" />
         <p>${newToy.likes}</p>
-        <button data id=${newToy.id} class="like-btn">Like</button>
+        <button data-id="${newToy.id}" class="like-btn">Like</button>
     </div>
 
       `
       toyCollection.innerHTML += newToyHTML
       // toyCollection.innerHTML += newToyHTML.join("")
       }) 
+    }
+    toyCollection.addEventListener("click", (e) => {
+      
+      
+      if (e.target.className === "like-btn") {
+        // /traverses the DOM
+        let currentLikes = 
+        parseInt(e.target.previousElementSibling.innerText)
+        let newLikes = currentLikes + 1
+        e.target.previousElementSibling.innerText = newLikes 
+        
+
+        fetch(`http://localhost:3000/toys/:${e.target.id}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+          body: JSON.stringify({
+            likes: newLikes 
+        })
+        
+        })
+      }
+    
     })
+
+      
+      
     addBtn.addEventListener("click", () => {
       // hide & seek with the form
       
       addToy = !addToy;
       if (addToy) {
         toyForm.style.display = "block";
+        toyForm.addEventListener("submit", event=> {
+          event.preventDefault()
+          newToy(event.target)
+        })
       } else {
         toyForm.style.display = "none";
       }
-    })
-  })
-
+    }
+  
+  )
+})
 
